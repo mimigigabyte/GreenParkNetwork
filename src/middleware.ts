9faf84957367abec7df5c614b039a411f9d8b14f
Dylan <shooterforever@gmail.com>
2025-09-22 10:24:32 +0800

    fix(mobile-h5)

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,6 +9,6 @@ export default createIntlMiddleware({
 export const config = {
   matcher: [
     // 匹配所有路径除了静态文件、API路由和管理员路径
-    '/((?!api|admin|_next/static|_next/image|favicon.ico|manifest.json|images|icon-).*)'
+    '/((?!api|admin|_next/static|_next/image|favicon.ico|manifest.json|images|icon-|MP_verify).*)'
   ]
 }
